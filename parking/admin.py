25f5eb0de6d98db1fdc378fe5100d570b14012fb
Andrey <foripoker@gmail.com>
2025-03-22 21:21:40 +0300

    add search in historyv and last 20 history in Passes

--- parking/admin.py
+++ parking/admin.py
@@ -1,9 +1,46 @@
 from django.contrib import admin
+from django.forms.models import BaseInlineFormSet
 from . import models
 
 
 class HistoryAdmin(admin.ModelAdmin):
-    list_display = ("parking_pass", "created_at")
+    list_display = ("parking_pass", "get_car", "get_car_number", "created_at")
+    search_fields = (
+        "parking_pass__car",
+        "parking_pass__car_number",
+        "parking_pass__name",
+        "created_at",
+    )
+
+    @admin.display(description="Номер автомобиля")
+    def get_car_number(self, obj):
+        return obj.parking_pass.car_number
+
+    @admin.display(description="Автомобиль")
+    def get_car(self, obj):
+        return obj.parking_pass.car
+
+
+class HistoryFormSet(BaseInlineFormSet):
+    def get_queryset(self):
+        qs = super(HistoryFormSet, self).get_queryset()
+        return qs[:20]
+
+
+class HistoryInline(admin.TabularInline):
+    model = models.History
+    formset = HistoryFormSet
+    fields = [
+        "created_at",
+    ]
+    ordering = ("-created_at",)
+    readonly_fields = [
+        "created_at",
+    ]
+    can_delete = False
+    max_num = 0
+    extra = 0
+    show_change_link = False
 
 
 class PassAdmin(admin.ModelAdmin):
@@ -12,6 +49,7 @@ class PassAdmin(admin.ModelAdmin):
     list_per_page = 20
     list_filter = ("service",)
     list_display_links = ("name",)
+    inlines = [HistoryInline]
 
 
 # Register your models here.
